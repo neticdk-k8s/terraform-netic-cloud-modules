131270c3d84082ea686e4cf7af52c869574bf5af
Tom Bramsen <tbr@netic.dk>
2026-06-12 10:42:03 +0200

    more detailed tshirts

--- modules/kubernetes/wrapper/locals.tf
+++ modules/kubernetes/wrapper/locals.tf
@@ -12,14 +12,42 @@ locals {
   ## or https://www.ovhcloud.com/en/public-cloud/prices/
   node_sku_mapping = {
     azure = {
-      small  = "Standard_B2ms"
-      medium = "Standard_D2s_v5"
-      large  = "Standard_D4s_v5"
+      # TEST & UDVIKLING (Burstable - B-serien)
+      test-small    = "Standard_B1ms"       # 1 vCPU / 2 GiB RAM
+      test-medium   = "Standard_B2s"        # 2 vCPU / 4 GiB RAM
+      test-large    = "Standard_B2ms"       # 2 vCPU / 8 GiB RAM
+      test-xl       = "Standard_B4ms"       # 4 vCPU / 16 GiB RAM
+
+      # PRODUCTION GENERAL PURPOSE (1:4 ratio - AMD v6)
+      small         = "Standard_D2ads_v6"   # 2 vCPU / 8 GiB RAM
+      medium        = "Standard_D4ads_v6"   # 4 vCPU / 16 GiB RAM
+      large         = "Standard_D8ads_v6"   # 8 vCPU / 32 GiB RAM
+      xl            = "Standard_D16ads_v6"  # 16 vCPU / 64 GiB RAM
+
+      # PRODUCTION MEMORY OPTIMIZED (1:8 ratio - AMD v6)
+      memory-small  = "Standard_E4ads_v6"   # 4 vCPU / 32 GiB RAM
+      memory-medium = "Standard_E8ads_v6"   # 8 vCPU / 64 GiB RAM
+      memory-large  = "Standard_E16ads_v6"  # 16 vCPU / 128 GiB RAM
+      memory-xl     = "Standard_E32ads_v6"  # 32 vCPU / 256 GiB RAM
     }
     ovh = {
-      small  = "d2-4"
-      medium = "b3-16"
-      large  = "b2-30"
+      # TEST & UDVIKLING (Shared / Discovery - d2-serien)
+      test-small    = "d2-2"                # 1 vCPU / 2 GiB RAM
+      test-medium   = "d2-4"                # 2 vCPU / 4 GiB RAM
+      test-large    = "d2-8"                # 4 vCPU / 8 GiB RAM
+      test-xl       = "r3-16"               # 2 vCPU / 16 GiB RAM (not really a test SKU but we need something for 16 GiB RAM in testing)
+
+      # PRODUCTION GENERAL PURPOSE (1:4 ratio - b3 NVMe)
+      small         = "b3-8"                # 2 vCPU / 8 GiB RAM
+      medium        = "b3-16"               # 4 vCPU / 16 GiB RAM
+      large         = "b3-32"               # 8 vCPU / 32 GiB RAM
+      xl            = "b3-64"               # 16 vCPU / 64 GiB RAM
+
+      # PRODUCTION MEMORY OPTIMIZED (1:8 ratio - r3 NVMe)
+      memory-small  = "r3-32"               # 4 vCPU / 32 GiB RAM
+      memory-medium = "r3-64"               # 8 vCPU / 64 GiB RAM
+      memory-large  = "r3-128"              # 16 vCPU / 128 GiB RAM
+      memory-xl     = "r3-256"              # 32 vCPU / 256 GiB RAM
     }
   }
 
